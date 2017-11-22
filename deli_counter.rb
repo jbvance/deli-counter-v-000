841146783e97d8f06668510cc1b69adeaed9047e
Jason Vance <jbvance@gmail.com>
2017-11-22 13:21:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+
+def take_a_number(katz_deli, name)
+  place_in_line = katz_deli.size == 0 ? 1 : katz_deli.size
+  puts "Welcome, #{name}. You are number #{place_in_line} in line."
+end
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    return "The line is currently empty."
+  end
+
+  tmp = "The line is currently: "
+  katz_deli.each_with_index do |name, index|
+    tmp += "#{index + 1}. #{name} "
+  end
+  tmp.strip!    
+end
+
